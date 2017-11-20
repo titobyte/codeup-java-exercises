ce54196137fa72e11efcda2d0b9b4e12e734e4cf
Tito Valiente <titolvaliente@gmail.com>
2017-11-20 14:47:39 -0600

    finished string exercise

--- src/Bob.java
+++ src/Bob.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args) {
-        
+        String bobResponse;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Talk to Bob");
+        bobResponse = sc.nextLine();
+
+        if (bobResponse.endsWith("?")) {
+            System.out.println("Sure");
+
+        }   else if (bobResponse.endsWith("!")){
+            System.out.println("Whoa chill out!");
+        }   else if (bobResponse.equals("")){
+            System.out.println("Fine. Be that way!");
+        }   else  {
+            System.out.println("Whatever");
+        }
+
     }
 }
