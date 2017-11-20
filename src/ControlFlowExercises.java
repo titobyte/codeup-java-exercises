0f044801d82559de6079c5a604134c05b4f6dbd8
Tito Valiente <titolvaliente@gmail.com>
2017-11-20 13:55:02 -0600

    finished control exercise and starting string exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,11 +1,64 @@
 public class ControlFlowExercises {
 
     public static void main(String[] args) {
-        int i = 5;
 
-        while(i <= 15) {
-            System.out.println(i);
-            i++;
-        }
+        //while loop
+//        int i = 5;
+//
+//        while(i <= 15) {
+//            System.out.print(i + " ");
+//            i++;
+
+        //do while  loop
+//        int i = 0;
+//
+//        do {
+//
+//            i++;
+//            i++;
+//            System.out.println(i);
+//
+//        } while (i <= 98);
+
+        //Alter loop by 5
+
+//        int i = 100;
+//
+//        do {
+//
+//            i -= 5;
+//            System.out.println(i);
+//
+//        } while (i >= -10);
+
+        //Alter to start at 2
+
+//          long i = 2;
+//        do {
+//
+//            System.out.println(i);
+//          i *= i
+//        } while (i <= 1000000);
+
+
+        //FizzBuzz
+      for (int i = 1; i <= 100; i++) {
+
+          if (i % 3 == 0 && i % 5 == 0) {
+              System.out.println("FizzBuzz");
+          } else if (i % 3 == 0) {
+              System.out.println("Fizz");
+          } else if (i % 5 == 0){
+              System.out.println("Buzz");
+          }  else {
+              System.out.println(i);
+          }
+
+      }
+        String firstName = "Fred";
+        short memberId = 1425;
+        System.out.printf("Member " + firstName+" "+ "---- " +"#"+memberId);
+
     }
 }
+
---
+++ src/StringExercise.java
@@ -0,0 +1,18 @@
+public class StringExercise {
+    public static void main(String[] args) {
+
+//        String message = "We don't need no education \nWe don't need no thought control";
+//        System.out.println(message);
+
+//        String message = "Check \"this\" out!, \"s inside of \"s!";
+//        System.out.println(message);
+
+//        String message = "In windows, the main drive is usually C:\\ ";
+//        System.out.println(message);
+//
+//        String message = "I can do backslashes \\, double backslashes \\\\,  \nand the amazing triple backslash \\\\\\!";
+//        System.out.println(message);
+
+
+    }
+}
